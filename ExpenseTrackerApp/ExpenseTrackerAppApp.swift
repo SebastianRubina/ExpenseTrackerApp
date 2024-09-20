d2ff86a81e0d6bd23c71702fea056b694b40c625
Sebastian Rubina <sebastian.rubina@icloud.com>
2024-09-19 21:07:15 -0400

    Initial Commit

---
+++ ExpenseTrackerApp/ExpenseTrackerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpenseTrackerAppApp.swift
+//  ExpenseTrackerApp
+//
+//  Created by Sebastián Rubina on 2024-09-19.
+//
+
+import SwiftUI
+
+@main
+struct ExpenseTrackerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
